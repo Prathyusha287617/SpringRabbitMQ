9b34fb8326dd622189322a837fbf7b6de4ea29f0
Prathyusha287617 <prathyusha.pg@ust.com>
2024-10-06 19:41:08 +0530

    added files

---
+++ spring-rabbitmq-consumer/src/main/java/com/spring_rabbitmq_consumer/CustomMessage.java
@@ -0,0 +1,16 @@
+package com.spring_rabbitmq_consumer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomMessage {
+    private String messageId;
+    private String message;
+    private Date messageDate;
+}
---
+++ spring-rabbitmq-producer/src/main/java/com/spring_rabbitmq_producer/CustomMessage.java
@@ -0,0 +1,18 @@
+package com.spring_rabbitmq_producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomMessage {
+    private String messageId;
+    private String message;
+    private Date messageDate;
+}
+
+
